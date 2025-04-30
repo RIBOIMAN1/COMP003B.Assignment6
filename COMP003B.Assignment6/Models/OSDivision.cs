ba3c40f69c0d4cb46ca303313c6c3aea0e43587c
RIBOIMAN1 <rileybenson@my.whccd.edu>
2025-04-30 03:39:40 -0700

    Added code that will show the user a technician, their info, and what operating system that they specialize in working on. Also tweaked "secrets.json " to define a database connection string.

---
+++ COMP003B.Assignment6/Models/OSDivision.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment6.Models
+{
+	public class OSDivision
+	{
+		public int CourseId { get; set; }
+
+		[Required]
+		public string Title { get; set; }
+
+		public virtual ICollection<OSDivision>? OSDivisions { get; set; }
+	}
+}
---
+++ COMP003B.Assignment6/Models/OSExperience.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment6.Models
+{
+	public class OSExperience
+	{
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Required]
+		public string OSType { get; set; }
+		public virtual Technician? Technician { get; set; }
+		public virtual OSDivision? OSDivision { get; set; }
+	}
+}
